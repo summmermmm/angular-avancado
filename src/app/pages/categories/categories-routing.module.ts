40d1843892cb62ec360571dfd28aa095cc5182b1
Henning Summer <henningsummer@mudameumundo.com.br>
2021-07-09 21:43:14 -0300

    add categories

---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories-form/categories-form.component';
+
+const routes: Routes = [
+  {path: '', component: CategoriesListComponent}, //pagina de listagem
+  {path: 'new', component: CategoriesFormComponent}, //rota para criar um novo no formulário
+  {path: ':id/edit', component: CategoriesFormComponent}, //rota para editar um novo no formulário
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule { }
